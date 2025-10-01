167cdc8b64f2b2d9ad83f30bf34ec322793a0ff3
Mohamad <M.Beheshti148@gmail.com>
2025-10-01 20:30:18 +0330

    added post model

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from Accounts.models import User
 
-# Create your models here.
+class CategoryModel(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        db_table = 'category'
+
+    def __str__(self):
+        return self.title
+
+class PostModel(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='posts/', null=True, blank=True)
+    content = models.TextField()
+    category = models.ForeignKey(CategoryModel, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'post'
+
+    def __str__(self):
+        if len(self.content) <= 30: return self.content
+        return self.content[:30]
+
